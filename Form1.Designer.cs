738e84b375172f3bc00a298b1b6f60095feb5bdc
Maxim <jidtirar@gmail.com>
2021-12-21 19:44:05 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace OOP_42
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_A = new System.Windows.Forms.TextBox();
+            this.tb_B = new System.Windows.Forms.TextBox();
+            this.tb_C = new System.Windows.Forms.TextBox();
+            this.num_A = new System.Windows.Forms.NumericUpDown();
+            this.num_B = new System.Windows.Forms.NumericUpDown();
+            this.num_C = new System.Windows.Forms.NumericUpDown();
+            this.track_A = new System.Windows.Forms.TrackBar();
+            this.track_B = new System.Windows.Forms.TrackBar();
+            this.track_C = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.num_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_C)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_B)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_C)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_A
+            // 
+            this.tb_A.Location = new System.Drawing.Point(104, 127);
+            this.tb_A.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_A.Name = "tb_A";
+            this.tb_A.Size = new System.Drawing.Size(101, 20);
+            this.tb_A.TabIndex = 0;
+            this.tb_A.Text = "0";
+            this.tb_A.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_A_KeyDown);
+            // 
+            // tb_B
+            // 
+            this.tb_B.Location = new System.Drawing.Point(273, 127);
+            this.tb_B.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_B.Name = "tb_B";
+            this.tb_B.Size = new System.Drawing.Size(101, 20);
+            this.tb_B.TabIndex = 1;
+            this.tb_B.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_B_KeyDown);
+            // 
+            // tb_C
+            // 
+            this.tb_C.Location = new System.Drawing.Point(448, 127);
+            this.tb_C.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_C.Name = "tb_C";
+            this.tb_C.Size = new System.Drawing.Size(101, 20);
+            this.tb_C.TabIndex = 2;
+            this.tb_C.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_C_KeyDown);
+            // 
+            // num_A
+            // 
+            this.num_A.Location = new System.Drawing.Point(104, 177);
+            this.num_A.Margin = new System.Windows.Forms.Padding(2);
+            this.num_A.Name = "num_A";
+            this.num_A.Size = new System.Drawing.Size(101, 20);
+            this.num_A.TabIndex = 3;
+            this.num_A.ValueChanged += new System.EventHandler(this.num_A_ValueChanged);
+            // 
+            // num_B
+            // 
+            this.num_B.Location = new System.Drawing.Point(273, 177);
+            this.num_B.Margin = new System.Windows.Forms.Padding(2);
+            this.num_B.Name = "num_B";
+            this.num_B.Size = new System.Drawing.Size(101, 20);
+            this.num_B.TabIndex = 4;
+            this.num_B.ValueChanged += new System.EventHandler(this.num_B_ValueChanged);
+            // 
+            // num_C
+            // 
+            this.num_C.Location = new System.Drawing.Point(448, 177);
+            this.num_C.Margin = new System.Windows.Forms.Padding(2);
+            this.num_C.Name = "num_C";
+            this.num_C.Size = new System.Drawing.Size(101, 20);
+            this.num_C.TabIndex = 5;
+            this.num_C.ValueChanged += new System.EventHandler(this.num_C_ValueChanged);
+            // 
+            // track_A
+            // 
+            this.track_A.LargeChange = 50;
+            this.track_A.Location = new System.Drawing.Point(104, 227);
+            this.track_A.Margin = new System.Windows.Forms.Padding(2);
+            this.track_A.Maximum = 100;
+            this.track_A.Name = "track_A";
+            this.track_A.Size = new System.Drawing.Size(101, 45);
+            this.track_A.SmallChange = 10;
+            this.track_A.TabIndex = 6;
+            this.track_A.Scroll += new System.EventHandler(this.track_A_Scroll);
+            // 
+            // track_B
+            // 
+            this.track_B.Location = new System.Drawing.Point(273, 227);
+            this.track_B.Margin = new System.Windows.Forms.Padding(2);
+            this.track_B.Maximum = 100;
+            this.track_B.Name = "track_B";
+            this.track_B.Size = new System.Drawing.Size(101, 45);
+            this.track_B.TabIndex = 7;
+            this.track_B.Scroll += new System.EventHandler(this.track_B_Scroll);
+            // 
+            // track_C
+            // 
+            this.track_C.Location = new System.Drawing.Point(448, 227);
+            this.track_C.Margin = new System.Windows.Forms.Padding(2);
+            this.track_C.Maximum = 100;
+            this.track_C.Name = "track_C";
+            this.track_C.Size = new System.Drawing.Size(101, 45);
+            this.track_C.TabIndex = 8;
+            this.track_C.Scroll += new System.EventHandler(this.track_C_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 340);
+            this.Controls.Add(this.track_C);
+            this.Controls.Add(this.track_B);
+            this.Controls.Add(this.track_A);
+            this.Controls.Add(this.num_C);
+            this.Controls.Add(this.num_B);
+            this.Controls.Add(this.num_A);
+            this.Controls.Add(this.tb_C);
+            this.Controls.Add(this.tb_B);
+            this.Controls.Add(this.tb_A);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.num_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_C)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_B)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_C)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_A;
+        private System.Windows.Forms.TextBox tb_B;
+        private System.Windows.Forms.TextBox tb_C;
+        private System.Windows.Forms.NumericUpDown num_A;
+        private System.Windows.Forms.NumericUpDown num_B;
+        private System.Windows.Forms.NumericUpDown num_C;
+        private System.Windows.Forms.TrackBar track_A;
+        private System.Windows.Forms.TrackBar track_B;
+        private System.Windows.Forms.TrackBar track_C;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_42
+{
+    public partial class Form_main : Form
+    {
+        Model modelA;
+        Model modelB;
+        Model modelC;
+
+        public Form_main()
+        {
+            InitializeComponent();
+            modelA = new Model();
+            modelB = new Model();
+            modelC = new Model();
+            modelA.observer += new System.EventHandler(this.UpdateFromModel);
+            modelB.observer += new System.EventHandler(this.UpdateFromModel2);
+            modelC.observer += new System.EventHandler(this.UpdateFromModel3);
+            modelC.setValue(100);
+        }
+
+
+      
+        private void num_A_ValueChanged(object sender, EventArgs e)
+        {
+            modelA.setValue(Decimal.ToInt32(num_A.Value));
+        }
+
+        private void tb_A_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                modelA.setValue(Int32.Parse(tb_A.Text));
+            }
+        }
+        private void track_A_Scroll(object sender, EventArgs e)
+        {
+            modelA.setValue(track_A.Value);
+        }
+        private void UpdateFromModel(object sender,EventArgs e)
+        {
+            tb_A.Text = modelA.getValue().ToString();
+            num_A.Value = modelA.getValue();
+            track_A.Value = modelA.getValue();
+            modelB.setMin(modelA.getValue());
+
+        }
+
+        private void num_B_ValueChanged(object sender, EventArgs e)
+        {
+            modelB.setValue(Decimal.ToInt32(num_B.Value));
+        }
+
+        private void tb_B_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                modelB.setValue(Int32.Parse(tb_B.Text));
+            }
+
+        }
+
+        private void track_B_Scroll(object sender, EventArgs e)
+        {
+            modelB.setValue(track_B.Value);
+        }
+        private void UpdateFromModel2(object sender, EventArgs e)
+        {
+            tb_B.Text = modelB.getValue().ToString();
+            num_B.Value = modelB.getValue();
+            track_B.Value = modelB.getValue();
+            modelC.setMin(modelB.getValue());
+            modelA.setMax(modelB.getValue());
+
+        }
+
+        private void num_C_ValueChanged(object sender, EventArgs e)
+        {
+            modelC.setValue(Decimal.ToInt32(num_C.Value));
+        }
+
+        private void tb_C_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                modelC.setValue(Int32.Parse(tb_C.Text));
+            }
+
+        }
+
+        private void track_C_Scroll(object sender, EventArgs e)
+        {
+            modelC.setValue(track_C.Value);
+        }
+        private void UpdateFromModel3(object sender, EventArgs e)
+        {
+            tb_C.Text = modelC.getValue().ToString();
+            num_C.Value = modelC.getValue();
+            track_C.Value = modelC.getValue();            
+            modelB.setMax(modelC.getValue());            
+
+        }
+    }
+}
+
+public class Model
+{
+    private int value, max, min;
+        
+    public System.EventHandler observer;  
+    public Model()
+    {
+        value = 0;
+        max = 100;
+        min = 0;
+    }
+    public void setValue(int _value)
+    {
+        if(_value <= max && _value >= min)
+        {
+            value = _value;   
+        }        
+        observer.Invoke(this, null);
+    }    
+    public int  getValue()
+    {
+        return value;
+    }
+    public void setMax(int _max)
+    {
+        max = _max;
+    }
+    public void setMin(int _min)
+    {
+        min = _min;
+    }
+}
